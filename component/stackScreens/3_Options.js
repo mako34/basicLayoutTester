9e2f5894a1606a980f45cf898aeebf252bc18d04
mako <manuel.betancurt@smokeball.com>
2018-01-16 10:40:51 +1100

    switch added 3_options

--- component/stackScreens/3_Options.js
+++ component/stackScreens/3_Options.js
@@ -9,11 +9,14 @@ import {
   Button
 } from "react-native";
 
+import { Switch } from "react-native-switch";
+
 var { height } = Dimensions.get("window");
 var topSeparatorHeight = 35;
 var boxUserHeight = 50;
 var boxFooterHeight = 80;
-var contentHeight = height - (topSeparatorHeight + boxUserHeight+ boxFooterHeight);
+var contentHeight =
+  height - (topSeparatorHeight + boxUserHeight + boxFooterHeight);
 
 export default class VerticalStackLayout extends Component {
   render() {
@@ -21,14 +24,12 @@ export default class VerticalStackLayout extends Component {
 
     return (
       <View style={styles.container}>
-
-
-      {/* separator */}
-      <View
+        {/* separator */}
+        <View
           style={{
             width: Dimensions.get("window").width,
             height: topSeparatorHeight,
-            backgroundColor: "skyblue",
+            backgroundColor: "white",
             alignItems: "center",
             justifyContent: "center"
           }}
@@ -53,9 +54,8 @@ export default class VerticalStackLayout extends Component {
           </TouchableHighlight>
         </View>
 
-        
         <View style={[styles.boxOptions]}>
-        <Button
+          <Button
             onPress={() =>
               navigate("ScreenParticipants", { screen: "ScreenSecurity Two" })
             }
@@ -63,31 +63,50 @@ export default class VerticalStackLayout extends Component {
             color="black"
           />
 
-          <Button
-            onPress={() => alert("Log out son")}
-            title="View deleted messages"
-            color="black"
-          />
+            <View style={styles.cellType}>
+
+            <Button
+              onPress={() => alert("Log out son")}
+              title="View deleted messages"
+              color="black"
+            />
+
+            <Switch
+              value={true}
+              onValueChange={val => console.log(val)}
+              disabled={false}
+              activeText={"On"}
+              inActiveText={"Off"}
+              circleSize={30}
+              barHeight={20}
+              circleBorderWidth={3}
+              backgroundActive={"green"}
+              backgroundInactive={"gray"}
+              circleActiveColor={"#30a566"}
+              circleInActiveColor={"#000000"}
+            />
+
+            </View>
+
+
         </View>
 
-{/* bottom buttons */}
+        {/* bottom buttons */}
         <View style={[styles.boxFooter]}>
-          <Button style={styles.bottomButton}
+          <Button
             onPress={() =>
               navigate("ScreenSecurity", { screen: "ScreenSecurity Two" })
             }
+            style={styles.bottomButton}
             title="Security"
-            color="blue"
           />
 
           <Button
             onPress={() => alert("Log out son")}
             title="Sign out"
-            color="blue"
+            style={styles.bottomButton}
           />
         </View>
-
-
       </View>
     );
   }
@@ -107,18 +126,25 @@ const styles = StyleSheet.create({
   },
   boxOptions: {
     height: contentHeight,
-    backgroundColor: "#ff0000"
+    backgroundColor: "white"
   },
   boxFooter: {
     height: boxFooterHeight,
-    backgroundColor: "#00ff00",
-    
+    backgroundColor: "white"
   },
   userIcon: {
     height: 30
   },
+  //fix!
   bottomButton: {
-    textAlign: 'left',
+    width: "100%",
+    height: "30%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cellType:{
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
- 
