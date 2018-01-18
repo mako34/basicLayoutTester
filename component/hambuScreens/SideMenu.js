b19f3398a1ba7537f3d7cb4904cd3f5b8f02b681
mako <manuel.betancurt@smokeball.com>
2018-01-19 10:48:54 +1100

    drawer layout wip

--- component/hambuScreens/SideMenu.js
+++ component/hambuScreens/SideMenu.js
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
-import styles from "./SideMenu.style";
+// import styles from "./SideMenu.style";
 import { NavigationActions } from "react-navigation";
 import {
-    StyleSheet,
+  StyleSheet,
   ScrollView,
   Text,
   View,
@@ -11,12 +11,15 @@ import {
   TouchableHighlight
 } from "react-native";
 
+var { height } = Dimensions.get("window");
 
-
-var { height } = Dimensions.get('window');
- 
 var box_count = 3;
 var box_height = height / box_count;
+
+var segmentedSelectorHeight = 50;
+var drawerWidth = 160;
+var drawerHeight = 40;
+var selectorButtonsWidth = 120;
 
 class SideMenu extends Component {
   navigateToScreen = route => () => {
@@ -26,101 +29,144 @@ class SideMenu extends Component {
     this.props.navigation.dispatch(navigateAction);
   };
 
-//   render() {
-//     return (
-//       <View style={styles.container}>
+  //   render() {
+  //     return (
+  //       <View style={styles.container}>
 
-        
+  //         <View
+  //           style={{
+  //             borderBottomColor: "white",
+  //             borderBottomWidth: 1
+  //           }}
+  //         />
 
-//         <View
-//           style={{
-//             borderBottomColor: "white",
-//             borderBottomWidth: 1
-//           }}
-//         />
+  //         <ScrollView>
+  //           <View>
+  //             <Text style={styles.sectionHeadingStyle}>Section 1</Text>
+  //             <View style={styles.navSectionStyle}>
+  //               <Text
+  //                 style={styles.navItemStyle}
+  //                 onPress={this.navigateToScreen("Page1")}
+  //               >
+  //                 Page unu
+  //               </Text>
+  //             </View>
+  //           </View>
+  //           <View>
+  //             <Text style={styles.sectionHeadingStyle}>Section 2</Text>
+  //             <View style={styles.navSectionStyle}>
+  //               <Text
+  //                 style={styles.navItemStyle}
+  //                 onPress={this.navigateToScreen("Page2")}
+  //               >
+  //                 Page2
+  //               </Text>
+  //               <Text
+  //                 style={styles.navItemStyle}
+  //                 onPress={this.navigateToScreen("Page3")}
+  //               >
+  //                 Page3
+  //               </Text>
+  //             </View>
+  //           </View>
+  //         </ScrollView>
+  //       </View>
+  //     );
+  //   }
+  // }
 
-//         <ScrollView>
-//           <View>
-//             <Text style={styles.sectionHeadingStyle}>Section 1</Text>
-//             <View style={styles.navSectionStyle}>
-//               <Text
-//                 style={styles.navItemStyle}
-//                 onPress={this.navigateToScreen("Page1")}
-//               >
-//                 Page unu
-//               </Text>
-//             </View>
-//           </View>
-//           <View>
-//             <Text style={styles.sectionHeadingStyle}>Section 2</Text>
-//             <View style={styles.navSectionStyle}>
-//               <Text
-//                 style={styles.navItemStyle}
-//                 onPress={this.navigateToScreen("Page2")}
-//               >
-//                 Page2
-//               </Text>
-//               <Text
-//                 style={styles.navItemStyle}
-//                 onPress={this.navigateToScreen("Page3")}
-//               >
-//                 Page3
-//               </Text>
-//             </View>
-//           </View>
-//         </ScrollView> 
-//       </View>
-//     );
-//   }
-// }
-
-render() {
+  render() {
     return (
-        <View style={styles.container}>
+      <View style={styles.container}>
+        <View style={[styles.boxSelector]} />
 
-<View style={{ flex: 1, flexDirection: "row" }}>
-          <View style={styles.segmentedControlButton}>
-            <TouchableHighlight onPress={() => this._selectorPressed}>
-              <Text style={styles.segmentedText}>Go to...</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            backgroundColor: "red"
+          }}
+        >
+          <View
+            style={{
+              width: drawerWidth,
+              height: segmentedSelectorHeight,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "yellow"
+            }}
+          >
+            <TouchableHighlight
+              style={[styles.selectorButtonMessages]}
+              onPress={this.ShowHideTextComponentView}
+              underlayColor="gray"
+            >
+              <Text>Go to...</Text>
             </TouchableHighlight>
           </View>
-          <View style={styles.segmentedControlButton}>
-            <TouchableHighlight onPress={() => this._selectorPressed}>
-              <Text style={styles.segmentedText}>New...</Text>
+
+          <View
+            style={{
+              width: drawerWidth,
+              height: segmentedSelectorHeight,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "white"
+            }}
+          >
+            <TouchableHighlight
+              style={[styles.selectorButtonMessages]}
+              onPress={this.ShowHideTextComponentView}
+              underlayColor="gray"
+            >
+              <Text>New...</Text>
             </TouchableHighlight>
           </View>
         </View>
 
-
-            <View style={[styles.box, styles.box1]}></View>
-            <View style={[styles.box, styles.box2]}></View>
-            <View style={[styles.box, styles.box3]}></View>
-        </View>
+        <View style={[styles.box2]} />
+        <View style={[styles.box3]} />
+      </View>
     );
   }
 }
- 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     flexDirection: 'column'
-//   },
-//   box: {
-//     height: box_height
-//   },
-//   box1: {
-//     backgroundColor: '#2196F3'
-//   },
-//   box2: {
-//     backgroundColor: '#8BC34A'
-//   },
-//   box3: {
-//     backgroundColor: '#e3aa1a'
-//   }
-// });
 
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "orange"
+  },
+  boxSelector: {
+    height: 40,
+    backgroundColor: "blue"
+  },
+  box1: {
+    backgroundColor: "#2196F3",
+    height: 40
+  },
+  box2: {
+    backgroundColor: "#8BC34A",
+    height: 40
+  },
+  box3: {
+    backgroundColor: "#ff00ff",
+    height: 40
+  },
+  selectorButtonMessages: {
+    backgroundColor: "rgb(231,231,231)",
+    width: selectorButtonsWidth,
+    height: 30,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  selectorButtonFiles: {
+    backgroundColor: "rgb(244,244,244)",
+    width: selectorButtonsWidth,
+    height: 30,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
 
 SideMenu.propTypes = {
   navigation: PropTypes.object
