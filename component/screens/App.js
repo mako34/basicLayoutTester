f7c62df873f3bd64b355ac65ad79258c5821e90b
mako <manuel.betancurt@smokeball.com>
2018-01-23 14:09:35 +1100

    more refactoring UI files

--- component/screens/App.js
+++ component/screens/App.js
@@ -4,21 +4,18 @@ import { DrawerNavigator, StackNavigator} from 'react-navigation';
 import ScreenUno from './ScreenUno';
 import ScreenDos from './ScreenDos';
 
-import ScreenOne from '../stackScreens/ScreenOne';
-import ScreenTwo from '../stackScreens/ScreenOne';
-import ScreenThree from '../stackScreens/ScreenOne';
 
 import stackScreenNavigator from '../stackScreens/App';
 
 //
 
-import ScreenIndex from '../stackScreens/1_ScreenIndex';
-import ScreenMenu from '../stackScreens/2_SideMenu';
-import ScreenOptions from '../stackScreens/3_Options';
-import ScreenParticipants from '../stackScreens/4_Participants';
-import ScreenSecurity from '../stackScreens/5_Security';
-import ScreenGoTo from '../stackScreens/6_GoTo';
-import ScreenNewMatter from '../stackScreens/7_NewMatter';
+import ScreenIndex from './1_ScreenIndex';
+import ScreenMenu from './2_SideMenu';
+import ScreenOptions from './3_Options';
+import ScreenParticipants from './4_Participants';
+import ScreenSecurity from './5_Security';
+import ScreenGoTo from './6_GoTo';
+import ScreenNewMatter from './7_NewMatter';
 
 import SideMenu from './SideMenu';
 
